7dd3556042aba986fe538fd3f1bcbe85aaddbe6e
Jeremie Dimino <jdimino@janestreet.com>
2017-01-19 13:19:13 +0000

    114.28+32

--- test/test_patdiff_git_wrapper.ml
+++ test/test_patdiff_git_wrapper.ml
@@ -1,6 +1,6 @@
 open! Core.Std
 open! Async.Std
-open! Expect_test_helpers.Std
+open! Expect_test_helpers
 
 (* The patdiff devs don't review the patdiff-git-wrapper script. This test is to warn them
    of obvious mistakes, like silently changing the calling convention without updating the
