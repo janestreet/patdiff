8c6ca4412c01b80fc5ac742f9a66c2f92135a0ee
Jeet Dekivadia <jeet.university@gmail.com>
2026-05-30 00:35:15 -0700

    Report package version from Dune build info
    Signed-off-by: Jeet Dekivadia <jeet.university@gmail.com>

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,13 @@
 open Core
 
+let version =
+  match Build_info.V1.version () with
+  | None -> "n/a"
+  | Some version -> Build_info.V1.Version.to_string version
+;;
+
 let () =
-  let result = Result.try_with (fun () -> Command_unix.run Compare.command) in
+  let result = Result.try_with (fun () -> Command_unix.run ~version Compare.command) in
   match result with
   | Ok () -> ()
   | Error exn ->
